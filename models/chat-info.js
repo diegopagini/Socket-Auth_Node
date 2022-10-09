4611795978fb8f0e4ec13f5aab03fc20ac4a6be0
Diego Pagini <diegopaginidev@gmail.com>
2022-10-09 14:40:51 +0200

    chat class

---
+++ models/chat-info.js
@@ -0,0 +1,37 @@
+/** @format */
+
+class Message {
+	constructor(uid, name, message) {
+		this.uid = uid;
+		this.name = name;
+		this.message = message;
+	}
+}
+
+export class ChatMessage {
+	constructor() {
+		this.messages = [];
+		this.users = {};
+	}
+
+	get lastTen() {
+		this.messages = this.messages.splice(0, 10);
+		return this.messages;
+	}
+
+	get usersArr() {
+		return Object.values(this.users); // [{}, {}]
+	}
+
+	sendMessage(uid, name, message) {
+		this.messages.unshift(new Message(uid, name, message));
+	}
+
+	connectUser(user) {
+		this.users[user.id] = user;
+	}
+
+	disconnectUser(id) {
+		delete this.users[id];
+	}
+}
--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 /** @format */
 import * as Category from './category.js';
+import * as ChatMessage from './chat-info.js';
 import * as Product from './product.js';
 import * as Role from './role.js';
 import * as Server from './server.js';
 import * as User from './user.js';
 
-export { Category, Role, Server, User, Product };
+export { Category, Role, Server, User, Product, ChatMessage };
